38264511b35e9b065a9d3118f9b9ef566ce74dd5
Ollie Ford <me@ojford.com>
2016-02-13 00:46:25 +0000

    updates adblock-hosts for lighter weight download

--- adblock-hosts.rb
+++ adblock-hosts.rb
@@ -1,14 +1,20 @@
 class AdblockHosts < Formula
   desc "Block ads from hosts file, amalgamation of sources"
   homepage "http://github.com/StevenBlack/hosts"
-  url "https://github.com/OJFord/hosts/archive/v1.0.tar.gz"
-  sha256 "c89ecd5451e43a03e65e32380150d8a1954d73020b7ae336e9cb026b88315359"
+  url "https://github.com/OJFord/hosts/archive/v1.1.tar.gz"
+  sha256 "5c49762d1354c8f94aaa0e5383127be351121e7d38b32e7078aed49d978e135c"
 
   def install
     system "mv updateHostsFile.py adblock-hosts"
     system "chmod +x adblock-hosts"
     prefix.install Dir["*"]
     bin.install_symlink "#{prefix}/adblock-hosts"
+  end
+
+  def caveats; <<-EOS.undent
+    You now need to run `adblock-hosts` for the first time to download hosts
+      data. This script can be run as often as desired to update hosts.
+    EOS
   end
 
   test do
