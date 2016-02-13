0e1dfd6c63f8fd88e0ccb4a51cff7b1007547a01
Ollie Ford <me@ojford.com>
2016-02-13 03:05:40 +0000

    merges updates from Homebrew/homebrew#49040, now v2.0

--- diff-so-fancy.rb
+++ diff-so-fancy.rb
@@ -1,13 +1,12 @@
 class DiffSoFancy < Formula
-  desc "builds on the good-lookin' output of diff-highlight"
+  desc "Good-lookin' diffs with diff-highlight and more"
   homepage "https://github.com/stevemao/diff-so-fancy"
-  url "https://github.com/stevemao/diff-so-fancy/archive/v0.1.2.tar.gz"
-  sha256 "fa4ac51368ba6b942c6b346117e5f72cabb00ea3c29f834e3c4ca80972e19502"
+  url "https://github.com/stevemao/diff-so-fancy/archive/v0.2.0.tar.gz"
+  sha256 "9f23732b7f7cde8bbbb019b4e0473d37888a55afe1b9a07ad9c1bce8ccf2334b"
 
   depends_on "gnu-sed"
 
   def install
-    inreplace "diff-so-fancy", "sed", "gsed"
     libexec.install Dir["*"]
     bin.install_symlink libexec/"diff-so-fancy"
   end
