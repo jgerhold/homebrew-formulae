02baaec07980f225017a9113b25ad6918afcdbfe
Ollie Ford <me@ojford.com>
2017-01-15 06:11:54 +0000

    Bump kramdown to v0.1.1

--- kramdown.rb
+++ kramdown.rb
@@ -1,9 +1,9 @@
 class Kramdown < Formula
   desc "Markdown specification for CS/math/eng. note-taking"
   homepage "https://github.com/OJFord/kramdown"
-  url "https://github.com/OJFord/kramdown/releases/download/v0.1/kramdown"
-  version "0.1"
-  sha256 "0e87125667f1563aa6dcb975e5b1871aa05ae3f3de76aec77220c156271c0938"
+  url "https://github.com/OJFord/kramdown/releases/download/v0.1.1/kramdown"
+  version "0.1.1"
+  sha256 "a903191b673fe62c84534baf5aa678a513ea38b089ecb021ef0254776d3b135c"
 
   def install
     bin.install "kramdown"
