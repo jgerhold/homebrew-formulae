0f62cbc007185dc80cc20a02c732c6458e308f28
Ollie Ford <dev@ojford.com>
2017-09-09 19:08:16 +0100

    Add Alacritty

---
+++ alacrity.rb
@@ -0,0 +1,15 @@
+class Alacritty < Formula
+  desc "A cross-platform, GPU-accelerated terminal emulator"
+  homepage "https://github.com/jwilm/alacritty"
+  head "https://github.com/jwilm/alacritty.git", using: :git
+
+  depends_on "cmake" => :build
+  depends_on "rust" => :build
+  depends_on "fontconfig"
+
+  def install
+    system "make", "app"
+    bin.install "target/release/osx/Alacritty.app"
+    (share / "alacritty").install *Dir["alacritty*.yml"]
+  end
+end
